097e72ef0a9fe14750263f9d425e775d6bbe9f01
Songlu-UNC <songlu@unc.edu>
2025-10-22 11:39:00 -0400

    CHIP690 Project
    This is for the 690 project

---
+++ CHIP 690 /SQL/pgSQL.py
@@ -0,0 +1,17 @@
+import psycopg2
+import pandas as pd
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="CHIP-690-WEB",
+    user="postgres",
+    password="12Minecraft",
+    port="5432"  # Default is 5432 if not specified
+)
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM accounts")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
